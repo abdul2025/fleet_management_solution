76e125ba882e09a826fe279b7a248f7ac3a3c2e9
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2025-12-21 17:28:09 +0300

    disable db logger

--- src/FleetManagement.Application/Aircrafts/Handlers/AircraftCreatedHandler.cs
+++ src/FleetManagement.Application/Aircrafts/Handlers/AircraftCreatedHandler.cs
@@ -11,7 +11,7 @@ namespace FleetManagement.Application.Aircrafts.Handlers
         public void Handle(AircraftCreatedEvent @event)
         {
             // Your custom logic here
-                    Console.WriteLine($"Aircraft {@event.RegistrationNumber} created at {@event.OccurredOn:O}");
+            Console.WriteLine($"Aircraft {@event.RegistrationNumber} created at {@event.OccurredOn:O}");
         }
     }
 }
